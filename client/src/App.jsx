c4f29db1088b5c3b0ee49c02091617b4d0ffa750
derangee <daksh_vashishtha@srmap.edu.in>
2025-04-17 20:10:51 +0530

    auth - login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./components/login";
+import LoginPage from "./pages/login";
 
 function App() {
   return (
